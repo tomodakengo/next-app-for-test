163ac57660f26dedb270cbcca3c8b61bbf848180
tomodakengo <joa.tomoda@gmail.com>
2024-02-28 13:09:20 +0900

    fix tailwind-config

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Tests//</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
