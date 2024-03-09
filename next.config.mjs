e448f06a1997a5638cabad125f740ecbfdb30547
Tomoda Kengo <39323965+tomodakengo@users.noreply.github.com>
2024-03-09 14:45:05 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,12 @@ const SUB_DIRECTORY = "/next-app-for-test";
 const isProd = process.env.NODE_ENV == "production"
 
 const nextConfig = {
+  basePath: SUB_DIRECTORY,
   output: "export",
-  assetPrefix: isProd === "production" ? SUB_DIRECTORY : "",
-  publicRuntimeConfig: {
-    basePath: isProd ? SUB_DIRECTORY : "",
-  }
+  // assetPrefix: isProd === "production" ? SUB_DIRECTORY : "",
+  // publicRuntimeConfig: {
+  //   basePath: isProd ? SUB_DIRECTORY : "",
+  // }
 };
 
 export default nextConfig;
