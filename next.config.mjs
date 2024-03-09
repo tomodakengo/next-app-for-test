fa26213a6c3d1dc56b326203d21549efa6204579
Tomoda Kengo <39323965+tomodakengo@users.noreply.github.com>
2024-03-09 14:29:05 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/next-app-for-test" : ""
 };
 
 export default nextConfig;
