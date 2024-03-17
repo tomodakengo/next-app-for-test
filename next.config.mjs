ea26fa9198cd162ee19a4541a6df4315410a947d
tomodakengo <joa.tomoda@gmail.com>
2024-03-17 20:38:25 +0900

    add code spell checker

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const SUB_DIRECTORY = "/next-app-for-test";
-const isProd = process.env.NODE_ENV == "production"
+const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig = {
   basePath: SUB_DIRECTORY,
